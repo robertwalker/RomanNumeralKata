2c789fd0a4d0ef8fbb2136f43eb9a92befbd6d06
Robert Walker <robertwalker1@icloud.com>
2018-06-28 10:59:20 -0400

    Step 1: String concatination

--- swift/Roman.playground/Contents.swift
+++ swift/Roman.playground/Contents.swift
@@ -10,11 +10,20 @@
 import Foundation
 import XCTest
 
-//class RomanFormatter {
-//    func format(_ arabic: Int) -> String {
-//        return ""
-//    }
-//}
+class RomanFormatter {
+    func format(_ arabic: Int) -> String {
+        if arabic == 3 {
+            return "I" + "I" + "I"
+        }
+        if arabic == 2 {
+            return "I" + "I"
+        }
+        if arabic == 1 {
+            return "I"
+        }
+        return ""
+    }
+}
 
 class RomanFormatterTests: XCTestCase {
     var fmt: RomanFormatter!
@@ -26,6 +35,16 @@ class RomanFormatterTests: XCTestCase {
     func testConvert1() {
         let roman = fmt.format(1)
         XCTAssertEqual(roman, "I")
+    }
+
+    func testConvert2() {
+        let roman = fmt.format(2)
+        XCTAssertEqual(roman, "II")
+    }
+
+    func testConvert3() {
+        let roman = fmt.format(3)
+        XCTAssertEqual(roman, "III")
     }
 }
 
